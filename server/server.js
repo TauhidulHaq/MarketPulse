90b20dc0e3c1f6e247605360e31cc3a872cb2746
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-04-13 04:52:53 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const authRoutes = require('./routes/authRoutes');
 const shopRoutes = require('./routes/shopRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
 const customerRoutes = require('./routes/customerRoutes');
-
+const productRoutes = require('./routes/productRoutes');
+const reportRoutes = require('./routes/reportRoutes');
 const app = express();
 
 app.use(cors());
@@ -18,6 +19,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/shops', shopRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/customers', customerRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/reports', reportRoutes);
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Market Pulse API is running' });
