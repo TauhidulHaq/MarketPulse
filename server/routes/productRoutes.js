1c7a0e90ae500f6621ededf1de8e0177edf8597d
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-04-20 10:00:13 +0600

    Replace auth middleware with protect function

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const auth = require('../middleware/auth');
+const { protect } = require('../middleware/auth'); 
 
-router.get('/all', auth, productController.getAllProducts);
-router.get('/:id', auth, productController.getProductById);
-router.get('/', auth, productController.getProducts);
-router.patch('/:id/price', auth, productController.updatePrice);
+router.get('/all', protect, productController.getAllProducts);
+router.get('/:id', protect, productController.getProductById);
+router.get('/', protect, productController.getProducts);
+router.patch('/:id/price', protect, productController.updatePrice);
 
 module.exports = router;
