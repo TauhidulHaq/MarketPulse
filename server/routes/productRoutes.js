bec6534248e761ee1d7a64c383fba234a5f63912
hab-ib <skills.shikhi@gmail.com>
2026-04-20 09:36:37 +0600

    productroutes fix

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { protect } = require('../middleware/auth'); // Ensure this matches your file named 'auth.js'
+const auth = require('../middleware/auth');
 
-router.get('/all', protect, productController.getAllProducts);
-router.get('/:id', protect, productController.getProductById);
-router.get('/', protect, productController.getProducts);
-router.patch('/:id/price', protect, productController.updatePrice);
+router.get('/all', auth, productController.getAllProducts);
+router.get('/:id', auth, productController.getProductById);
+router.get('/', auth, productController.getProducts);
+router.patch('/:id/price', auth, productController.updatePrice);
 
 module.exports = router;
