987f393dc4913d3181e24b2286bc4db4d4bdf987
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-04-20 09:12:12 +0600

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { protect } = require('../middleware/auth');
-
+const { protect } = require('../middleware/auth'); // Ensure this matches your file named 'auth.js'
 
 router.get('/all', protect, productController.getAllProducts);
 router.get('/:id', protect, productController.getProductById);
-
-
 router.get('/', protect, productController.getProducts);
-
-
 router.patch('/:id/price', protect, productController.updatePrice);
 
 module.exports = router;
