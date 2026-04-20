1e32da2446fba3f3550a38c55d2f52ecedb30cd0
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-04-20 09:03:37 +0600

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { protect } = require('../middleware/auth'); 
-
+const { protect } = require('../middleware/auth');
 
 router.get('/all', protect, productController.getAllProducts);
 router.get('/:id', protect, productController.getProductById);
 router.get('/', protect, productController.getProducts);
-router.patch('/:id/price', protect, productController.updatePrice); 
+router.patch('/:id/price', protect, productController.updatePrice);
 
 module.exports = router;
