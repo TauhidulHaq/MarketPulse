ea81205d2cc654cc877579356cc4290c577024b4
hab-ib <skills.shikhi@gmail.com>
2026-05-03 17:08:05 +0600

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {//render frontend er jonno
-    rollupOptions: {
-      external: ['react/jsx-runtime'],
-    },
-  },
   server: {
     port: 5173,
     proxy: {
